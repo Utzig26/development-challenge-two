00d075e781b2f9510278a8675f51d4578f5bf9d1
gabriel <gabriel utzig>
2022-12-19 18:53:02 -0300

    add a error hrandler middleware

---
+++ back-end/medcloud-api/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+
+export function ErrorHandlerMiddleware(err: Error, req: Request, res: Response) {
+  res.status(500).json({ message: err.message });
+}
