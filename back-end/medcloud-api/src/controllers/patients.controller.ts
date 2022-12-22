c28619ae92b21bf08f6d7469e801b40ffa96f5e3
gabriel <gabriel utzig>
2022-12-21 22:37:30 -0300

    add PUT/PATCH Patient route | add pagination in GET Patients route

--- back-end/medcloud-api/src/controllers/patients.controller.ts
+++ back-end/medcloud-api/src/controllers/patients.controller.ts
@@ -1,21 +1,29 @@
 import { Request, Response, NextFunction} from 'express';
 
 import { PatientsService } from '../services/patients.service';
-import { CreatePatientDto } from '../dtos/createPatient.dto';
+import { PatientDto } from '../dtos/patient.dto';
+import { PartialPatientDto } from '../dtos/partialPatient.dto';
+
+// set default values for limit
+const DEFAULT_LIMIT = 10;
+
+// Find patient by id and return it or return 404
+async function findPatient(patientId: string, res:Response) {
+  const patient = await PatientsService.getPatient(patientId);
+  if(!patient) 
+    res
+      .json({
+        message: 'Patient not found'
+      })
+      .status(404).end();
+  else
+    return patient;
+}
 
 async function getPatient(req:Request, res:Response, next:NextFunction) {
   const patientId = req.params.id;
   try{
-    const patient = await PatientsService.getPatient(patientId);
-
-    if(!patient) {
-      return res
-        .json({
-          message: 'Patient not found'
-        })
-        .status(404).end();
-    }
-
+    const patient = await findPatient(patientId, res);
     res
       .json({
         message: 'Patient retrieved successfully',
@@ -28,22 +36,17 @@ async function getPatient(req:Request, res:Response, next:NextFunction) {
 }
 
 async function getPatients(req:Request, res:Response, next:NextFunction) {
+  const limit:number = req.query.limit? parseInt(req.query.limit as string) : DEFAULT_LIMIT;
+  const startAt:string|undefined = req.query.startAt? (req.query.startAt as string) : undefined;
+  
   try{
-    const patients = await PatientsService.getPatients();
-    
-    if(!patients) {
-      return res
-        .json({
-          message: 'Patients not found'
-        })
-        .status(404).end();
-    }
-
+    const patients = await PatientsService.getPatients(limit, startAt);
     res
       .json({
         message: 'Patients retrieved successfully',
         amount: patients.length,
-        patients: patients
+        patients: patients,
+        lastKey: patients.length < limit? null : patients[patients.length-1].id
       })
       .status(200).end();
   } catch (err) {
@@ -52,7 +55,7 @@ async function getPatients(req:Request, res:Response, next:NextFunction) {
 }
 
 async function createPatient(req:Request, res:Response, next:NextFunction) {
-  const patientDTO: CreatePatientDto = req.body;
+  const patientDTO: PatientDto = req.body;
   try{
     const newPatient = await PatientsService.createPatient(patientDTO);
     res
@@ -70,15 +73,7 @@ async function deletePatient(req:Request, res:Response, next:NextFunction) {
   const patientId = req.params.id;
   
   try{
-    const patient = await PatientsService.getPatient(patientId);
-    if(!patient) {
-      return res
-        .json({
-          message: 'Patient not found'
-        })
-        .status(404).end();
-    }
-
+    const patient = await findPatient(patientId, res);
     await PatientsService.deletePatient(patientId);
     res
       .json({
@@ -91,5 +86,38 @@ async function deletePatient(req:Request, res:Response, next:NextFunction) {
   }
 }
 
+async function patchPatient(req:Request, res:Response, next:NextFunction) {
+  const patientId = req.params.id;
+  const patientDTO:PartialPatientDto = req.body;
+  try{
+    await findPatient(patientId, res)
+    const patchedPatient = await PatientsService.patchPatient(patientId, patientDTO);
+    res
+      .json({
+        message: 'Patient patched successfully',
+        patient: patchedPatient
+      })
+      .status(200).end();
+  } catch (err) {
+    next(err);
+  }
+}
 
-export const PatientController = { getPatient, getPatients, createPatient, deletePatient };
+async function putPatient(req:Request, res:Response, next:NextFunction) {
+  const patientId = req.params.id;
+  const patientDTO:PatientDto = req.body;
+  try{
+    await findPatient(patientId, res)
+    const updatedPatient = await PatientsService.updatePatient(patientId, patientDTO);
+    res
+      .json({
+        message: 'Patient updated successfully',
+        patient: updatedPatient
+      })
+      .status(200).end();
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const PatientController = { getPatient, getPatients, createPatient, deletePatient, patchPatient, putPatient };
--- back-end/medcloud-api/src/routes/patients.route.ts
+++ back-end/medcloud-api/src/routes/patients.route.ts
@@ -2,11 +2,12 @@ import express from 'express';
 
 import { PatientController } from "../controllers/patients.controller";
 import { ValidadeDtoMiddleware } from '../middlewares/validateDto.middleware';
-import { CreatePatientDto } from '../dtos/createPatient.dto';
+import { PatientDto } from '../dtos/patient.dto';
+import { PartialPatientDto } from '../dtos/partialPatient.dto';
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/', ValidadeDtoMiddleware(CreatePatientDto));
+router.post('/', ValidadeDtoMiddleware(PatientDto));
 router.post('/', PatientController.createPatient );
 
 router.get('/', PatientController.getPatients);
@@ -14,5 +15,10 @@ router.get('/:id', PatientController.getPatient);
 
 router.delete('/:id', PatientController.deletePatient);
 
+router.patch('/:id', ValidadeDtoMiddleware(PartialPatientDto));
+router.patch('/:id', PatientController.patchPatient);
+
+router.put('/:id', ValidadeDtoMiddleware(PatientDto));
+router.put('/:id', PatientController.putPatient);
 
 export default router;
