592fe149ad0aa536d972fa84b47c48580ca18994
gabriel <gabriel utzig>
2022-12-23 23:49:58 -0300

    add header component

---
+++ front-end/medcloud-web/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import AppBar from '@mui/material/AppBar';
+import { Box, Toolbar } from '@mui/material';
+import MedCloudSvg from '../../assets/medcloud.svg'
+
+import './style.css';
+function Header() {
+
+  return (
+    <Box>
+      <AppBar component="nav" color="complementary">
+        <Toolbar className='header-body'>        
+          <img src={MedCloudSvg} className='logo-icon' />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
