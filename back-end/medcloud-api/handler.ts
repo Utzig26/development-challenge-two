f3fd64e0d06c8d017f3bb672634016bc78c53bb5
gabriel <gabriel utzig>
2022-12-27 17:25:03 -0300

    add cors config

--- back-end/medcloud-api/handler.ts
+++ back-end/medcloud-api/handler.ts
@@ -7,6 +7,12 @@ import { patientsRouter } from "./src/routes/patients.route.js";
 
 const app: Express = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+  });
 app.use(express.json());
 app.use('/patients', (req: Request, res: Response, next: NextFunction) => { patientsRouter(req, res, next) });
 
